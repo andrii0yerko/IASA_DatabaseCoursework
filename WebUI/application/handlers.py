9f159a997222274a4c1e05453b903b5ff5f5898b
Andrii Yerko <aerko12@gmail.com>
2021-05-17 21:31:13 +0300

    Database errors handler

--- WebUI/application/handlers.py
+++ WebUI/application/handlers.py
@@ -1,4 +1,5 @@
-from flask import render_template, flash
+from flask import render_template, flash, redirect, url_for, request
+from psycopg2.errors import CheckViolation, ForeignKeyViolation
 
 from application import app, db
 
@@ -18,3 +19,11 @@ def unauthorized(e):
 def internal_error(e):
     db.conn.rollback()
     return render_template("500.html")
+  
+  
+@app.errorhandler(ForeignKeyViolation)
+@app.errorhandler(CheckViolation)
+def handle_bad_request(e):
+    db.conn.rollback()
+    flash("Enter the correct values!")
+    return redirect(request.url)
